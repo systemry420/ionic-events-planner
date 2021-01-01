32cfda9dca4f479835a5b7bf98eca32bbcd46bd6
Hasan <systemry420@gmail.com>
2021-01-01 14:52:22 +0200

    add auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './pages/auth/auth.guard';
 
 const routes: Routes = [
   {
@@ -9,19 +10,23 @@ const routes: Routes = [
   },
   {
     path: 'user-profile',
-    loadChildren: () => import('./pages/user-profile/user-profile.module').then( m => m.UserProfilePageModule)
+    loadChildren: () => import('./pages/user-profile/user-profile.module').then( m => m.UserProfilePageModule),
+    canActivate: [ AuthGuard ]
   },
   {
     path: 'pages/schedule',
-    loadChildren: () => import('./pages/schedule/schedule.module').then( m => m.SchedulePageModule)
+    loadChildren: () => import('./pages/schedule/schedule.module').then( m => m.SchedulePageModule),
+    canActivate: [ AuthGuard ]
   },
   {
     path: 'pages/home',
-    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule),
+    canActivate: [ AuthGuard ]
   },
   {
     path: 'pages/preferences',
-    loadChildren: () => import('./pages/preferences/preferences.module').then( m => m.PreferencesPageModule)
+    loadChildren: () => import('./pages/preferences/preferences.module').then( m => m.PreferencesPageModule),
+    canActivate: [ AuthGuard ]
   },
   {
     path: 'auth',
